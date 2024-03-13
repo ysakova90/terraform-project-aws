f2771f1d32ce3b4eb7a0228f076df252e8b6fe8f
Aigul Ysakova <ysakova90@gmail.com>
2024-03-13 13:39:44 -0500

    provider added

--- provider.tf
+++ provider.tf
@@ -1,9 +1,5 @@
-resource "aws_vpc" "main" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
-
-  tags = {
-    Name = "main"
-  }
+# Configure the AWS Provider
+provider "aws" {
+  region = var.region
 }
 
