15f3daac99c36a08b8bd939dd0af9c3104da907a
Aigul Ysakova <ysakova90@gmail.com>
2024-03-14 14:53:54 -0500

    changed

--- template/main.tf
+++ template/main.tf
@@ -46,7 +46,6 @@ resource "aws_eip" "nat_gateway_eip" {
 }  
 
 resource "aws_nat_gateway" "nat_gateway" {
-  depends_on    = [aws_subnet.public_subnets]
   allocation_id = aws_eip.nat_gateway_eip.id
   subnet_id     = aws_subnet.public_subnets["public_subnet_1"].id
   tags          = var.tags
