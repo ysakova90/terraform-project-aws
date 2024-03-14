a814f9174ea22a5ffddd3de291484bbdc1f3b90b
Aigul Ysakova <ysakova90@gmail.com>
2024-03-14 14:02:41 -0500

    added subnets

--- template/variable.tf
+++ template/variable.tf
@@ -3,3 +3,43 @@ variable "cidr_block" {
   type        = string
   default     = ""
 }
+variable "instance_tenancy" {
+  description = "please provide instance_tenancy information"
+  type        = string
+  default     = ""
+}
+variable "tags" {
+  description = "Please specify tags"
+  type        = map(any)
+  default     = {}
+}
+variable "private_subnet1" {
+  description = "Please provide cidr block for subnet1"
+  type        = string
+  default     = ""
+}
+variable "private_subnet2" {
+  description = "Please provide cidr block for subnet2"
+  type        = string
+  default     = ""
+}
+variable "private_subnet3" {
+  description = "Please provide cidr block for subnet3"
+  type        = string
+  default     = ""
+}
+variable "public_subnet1" {
+  description = "Please provide cidr block for subnet1"
+  type        = string
+  default     = ""
+}
+variable "public_subnet2" {
+  description = "Please provide cidr block for subnet2"
+  type        = string
+  default     = ""
+}
+variable "public_subnet3" {
+  description = "Please provide cidr block for subnet3"
+  type        = string
+  default     = ""
+}
