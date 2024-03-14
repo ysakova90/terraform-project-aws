2a36e93cee7afa369c475bab9ac52e4c66ac92d7
Aigul Ysakova <ysakova90@gmail.com>
2024-03-14 14:08:46 -0500

    changed

--- template/main.tf
+++ template/main.tf
@@ -1,7 +1,5 @@
 resource "aws_vpc" "main" {
   cidr_block       = var.cidr_block   #string variable
-  instance_tenancy = var.instance_tenancy # string variable
-  tags             = var.tags
 
 }
 
