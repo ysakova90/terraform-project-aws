93b53b0dc3e93c2c4ef09a534d306411c75c43f4
Aigul Ysakova <ysakova90@gmail.com>
2024-03-14 15:02:11 -0500

    changed

--- template/main.tf
+++ template/main.tf
@@ -40,13 +40,19 @@ resource "aws_internet_gateway" "gw" {
   vpc_id     = aws_vpc.main.id
   tags       = var.tags
 }
-resource "aws_eip" "nat_gateway_eip" {
-  domain   = "vpc"  
-  tags     = var.tags
-}  
+resource "aws_eip" "nat" {
+  vpc      = true
+}
+ 
 
 resource "aws_nat_gateway" "nat_gateway" {
   allocation_id = aws_eip.nat_gateway_eip.id
   subnet_id     = aws_subnet.nat_gateway.id
   tags          = var.tags
+}
+resource "aws_nat_gateway" "example" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public1.id
+  depends_on = [aws_internet_gateway.gw]
+  tags   = var.tags
 }
