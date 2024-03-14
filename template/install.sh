d0dd5b48d54cf4234d586e0b9e7ed4b099e1dad7
Aigul Ysakova <ysakova90@gmail.com>
2024-03-14 18:15:12 +0000

    cleaning up

--- template/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-rm -rf ~/.tfenv
-git clone --depth=1 https://github.com/tfutils/tfenv.git ~/.tfenv
-echo 'export PATH="$HOME/.tfenv/bin:$PATH"' >> ~/.bash_profile
-echo 'export PATH=$PATH:$HOME/.tfenv/bin' >> ~/.bashrc
-mkdir -p $HOME/bin
-ln -s ~/.tfenv/bin/* $HOME/bin
