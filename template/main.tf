0130242ca62bec4f9d7a8696f0bb3974f4d5fd19
ysakova90 <153944222+ysakova90@users.noreply.github.com>
2024-03-14 20:34:11 -0500

    Update main.tf

--- template/main.tf
+++ template/main.tf
@@ -37,15 +37,28 @@ resource "aws_subnet" "private_subnet3" {
   tags       = var.tags
 }
 resource "aws_internet_gateway" "gw" {
-  vpc_id     = aws_vpc.main.id
-  tags       = var.tags
+ vpc_id = aws_vpc.main.id
+} 
+# Attach Internet Gateway to Public Subnets
+resource "aws_route_table" "public_subnet_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
 }
-resource "aws_eip" "main" {
-  domain   = "vpc"
-  tags     = var.tags
-}  
- resource "aws_nat_gateway" "natgw" {
-  allocation_id = "aws_eip.main.id"
-  subnet_id     = aws_subnet.puclic_subnet.id
-  tags   = var.tags
+resource "aws_route_table_association" "public1" {
+  subnet_id      = aws_subnet.public1.id
+  route_table_id = aws_route_table.public_subnet_rt.id
+}
+
+resource "aws_route_table_association" "public2" {
+  subnet_id      = aws_subnet.public2.id
+  route_table_id = aws_route_table.public_subnet_rt.id
+}
+
+resource "aws_route_table_association" "public3" {
+  subnet_id      = aws_subnet.public3.id
+  route_table_id = aws_route_table.public_subnet_rt.id
 }
