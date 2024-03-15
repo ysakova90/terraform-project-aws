7c4de20e85751267a27a4380e59d81216ed8c5a1
Aigul Ysakova <ysakova90@gmail.com>
2024-03-14 22:09:15 -0500

    changed

--- template/main.tf
+++ template/main.tf
@@ -34,7 +34,6 @@ resource "aws_subnet" "private_subnet2" {
 resource "aws_subnet" "private_subnet3" {
   vpc_id     = aws_vpc.main.id
   cidr_block = var.private_subnet3
-  tags       = var.tags
 }
 resource "aws_internet_gateway" "gw" {
  vpc_id = aws_vpc.main.id
@@ -47,12 +46,11 @@ resource "aws_route_table" "public_route_table" {
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.gw.id
-    tags       = var.tags
+  
   }
  }
 
 resource "aws_route_table" "private_route_table" {
   vpc_id       = aws_vpc.main.id
-  tags         = var.tags
 }
 
