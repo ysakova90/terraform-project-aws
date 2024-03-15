e284a59440ce6989712842079ea2f294a3fa8acd
Aigul Ysakova <ysakova90@gmail.com>
2024-03-14 21:01:44 -0500

    added rt

--- template/main.tf
+++ template/main.tf
@@ -49,3 +49,17 @@ resource "aws_route_table" "public_subnet_rt" {
   }
 
 }
+resource "aws_route_table_association" "public1" {
+  subnet_id      = aws_subnet.public1.id
+  route_table_id = aws_route_table.public_subnet_rt.id
+}
+
+resource "aws_route_table_association" "public2" {
+  subnet_id      = aws_subnet.public2.id
+  route_table_id = aws_route_table.public_subnet_rt.id
+}
+
+resource "aws_route_table_association" "public3" {
+  subnet_id      = aws_subnet.public3.id
+  route_table_id = aws_route_table.public_subnet_rt.id
+}
