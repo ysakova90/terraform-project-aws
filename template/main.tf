7a2e804d4da131fd91134c2d4a710979ff1a07d4
Aigul Ysakova <ysakova90@gmail.com>
2024-03-14 22:22:32 -0500

    added

--- template/main.tf
+++ template/main.tf
@@ -57,3 +57,11 @@ resource "aws_route_table_association" "public1" {
   subnet_id      = aws_subnet.public_subnet1.id
   route_table_id = aws_route_table.public_route_table.id
 }
+resource "aws_route_table_association" "public2" {
+  subnet_id      = aws_subnet.public_subnet2.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+resource "aws_route_table_association" "public3" {
+  subnet_id      = aws_subnet.public_subnet3.id
+  route_table_id = aws_route_table.public_route_table.id
+}
