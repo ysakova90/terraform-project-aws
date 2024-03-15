91f1d2d7d9bbbee13b32116f562d1d20d733cf41
Aigul Ysakova <ysakova90@gmail.com>
2024-03-14 22:30:37 -0500

    added

--- template/main.tf
+++ template/main.tf
@@ -53,6 +53,8 @@ resource "aws_route_table" "public_route_table" {
 resource "aws_route_table" "private_route_table" {
   vpc_id       = aws_vpc.main.id
 }
+
+  #Create route table associations 
 resource "aws_route_table_association" "public1" {
   subnet_id      = aws_subnet.public_subnet1.id
   route_table_id = aws_route_table.public_route_table.id
@@ -65,3 +67,15 @@ resource "aws_route_table_association" "public3" {
   subnet_id      = aws_subnet.public_subnet3.id
   route_table_id = aws_route_table.public_route_table.id
 }
+ resource "aws_route_table_association" "private1" {
+  subnet_id      = aws_subnet.private_subnet1.id
+  route_table_id = aws_route_table.private_route_table.id
+ }
+ resource "aws_route_table_association" "private2" {
+  subnet_id      = aws_subnet.private_subnet2.id
+  route_table_id = aws_route_table.private_route_table.id
+ }
+ resource "aws_route_table_association" "private3" {
+  subnet_id      = aws_subnet.private_subnet3.id
+  route_table_id = aws_route_table.private_route_table.id
+ }
