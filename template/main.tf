5ebd456632e371ade581bf1ede327210b1d21f9d
Aigul Ysakova <ysakova90@gmail.com>
2024-03-14 21:59:21 -0500

    cahnged

--- template/main.tf
+++ template/main.tf
@@ -52,9 +52,5 @@ resource "aws_route_table" "public_route_table" {
 resource "aws_route_table" "private_route_table" {
   vpc_id       = aws_vpc.main.id
 
-  route {
-    cidr_block      = "0.0.0.0/0"
-    nat_gateway_id  = aws_nat_gateway.nat_gateway.id
-  }
 }
 
