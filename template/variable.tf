52240545ece5dd2fa54e09ff3a90fe1a43a1ee14
Aigul Ysakova <ysakova90@gmail.com>
2024-03-14 20:16:57 -0500

    igw

--- template/variable.tf
+++ template/variable.tf
@@ -48,3 +48,30 @@ variable "private_subnet3" {
   type        = string
   default     = ""
 }
+resource "aws_internet_gateway" "gw" {
+ vpc_id = aws_vpc.Project.id
+ 
+}
+# Attach Internet Gateway to Public Subnets
+resource "aws_route_table" "public_subnet_rt" {
+  vpc_id = aws_vpc.Project.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+resource "aws_route_table_association" "public1" {
+  subnet_id      = aws_subnet.public1.id
+  route_table_id = aws_route_table.public_subnet_rt.id
+}
+
+resource "aws_route_table_association" "public2" {
+  subnet_id      = aws_subnet.public2.id
+  route_table_id = aws_route_table.public_subnet_rt.id
+}
+
+resource "aws_route_table_association" "public3" {
+  subnet_id      = aws_subnet.public3.id
+  route_table_id = aws_route_table.public_subnet_rt.id
+}
