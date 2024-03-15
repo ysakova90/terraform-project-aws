8d7cf7a6a51ffaca0fd8873ee6e00723969d126e
Aigul Ysakova <ysakova90@gmail.com>
2024-03-14 21:56:28 -0500

    cahnged

--- template/main.tf
+++ template/main.tf
@@ -40,26 +40,21 @@ resource "aws_internet_gateway" "gw" {
  vpc_id = aws_vpc.main.id
 } 
 # Attach Internet Gateway to Public Subnets
-resource "aws_route_table" "public_subnet_rt" {
+resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.main.id
 
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.gw.id
   }
+ }
 
-}
-resource "aws_route_table_association" "public1" {
-  subnet_id      = aws_subnet.public1.id
-  route_table_id = aws_route_table.public_subnet_rt.id
+resource "aws_route_table" "private_route_table" {
+  vpc_id       = aws_vpc.main.id
+
+  route {
+    cidr_block      = "0.0.0.0/0"
+    nat_gateway_id  = aws_nat_gateway.nat_gateway.id
+  }
 }
 
-resource "aws_route_table_association" "public2" {
-  subnet_id      = aws_subnet.public2.id
-  route_table_id = aws_route_table.public_subnet_rt.id
-}
-
-resource "aws_route_table_association" "public3" {
-  subnet_id      = aws_subnet.public3.id
-  route_table_id = aws_route_table.public_subnet_rt.id
-}
