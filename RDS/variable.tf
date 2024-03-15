0f63068cfc99aa7a0049444051ec1d5886f459ab
Aigul Ysakova <ysakova90@gmail.com>
2024-03-15 17:17:39 -0500

    added

---
+++ RDS/variable.tf
@@ -0,0 +1,9 @@
+variable "region" {
+  description = "Please provide a region name"
+  type        = string
+}
+variable "tags" {
+  description = "Please provide a tag for resources"
+  type        = map(any)
+  default     = {}
+}
