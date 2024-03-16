fc3634f55238005ef11072c3f0e7a4114c2c37c8
Aigul Ysakova <ysakova90@gmail.com>
2024-03-16 02:12:13 -0500

    added

--- RDS/main.tf
+++ RDS/main.tf
@@ -1,16 +1,9 @@
-data "aws_caller_identity" "current" {}
+resource "aws_db_subnet_group" "RDS_subnet_grp" {
+  subnet_ids = [
+    data.terraform_remote_state.backend.outputs.private_subnet1,
+    data.terraform_remote_state.backend.outputs.private_subnet2,
+    data.terraform_remote_state.backend.outputs.private_subnet3,
+  ]
 
-
-locals {
-  account_id = data.aws_caller_identity.current.account_id
-}
-
-
-data "terraform_remote_state" "backend" {
-  backend = "s3"
-  config = {
-    bucket = "tfstate-${local.account_id}"
-    key    = "tfstate-team1/dev/VPC"
-    region = "us-east-1"
-  }
+  tags = var.tags
 }
