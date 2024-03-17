87cdb08378584123289692d5a5dd1d606a8d788e
Aigul Ysakova <ysakova90@gmail.com>
2024-03-17 13:26:05 -0500

    add

--- RDS/main.tf
+++ RDS/main.tf
@@ -1,5 +1,15 @@
-##Create subnet group for RDS
+data "terraform_remote_state" "vpc" {
+  backend = "local"
+
+  config = {
+    bucket = "team1-aws-storage"
+    key    = ""path/to/my/tfstate""
+    region = "us-east-1"
+  }
+}
+
+
 resource "aws_db_subnet_group" "RDS_subnet_grp" {
-  name = project_subnet_group
-  subnet_ids = [aws_subnet.private_subnet1.id, aws_subnet.private_subnet2.id, aws_subnet.private_subnet3.id,]
+  subnet_ids = data.terraform_remote_state.backend.outputs.private_subnets
+
 }
