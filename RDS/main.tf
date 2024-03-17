1d3b8b20b20590bc5439665a7f3a6645601fd397
Aigul Ysakova <ysakova90@gmail.com>
2024-03-16 23:28:55 -0500

    changed

--- RDS/main.tf
+++ RDS/main.tf
@@ -1,5 +1,5 @@
 ##Create subnet group for RDS
 resource "aws_db_subnet_group" "RDS_subnet_grp" {
   name = project_subnet_group
-  subnet_ids = [aws_subnet.private1.id, aws_subnet.private2.id, aws_subnet.private3.id]
-  
+  subnet_ids = [aws_subnet.private_subnet1.id, aws_subnet.private_subnet2.id, aws_subnet.private_subnet3.id,]
+}
