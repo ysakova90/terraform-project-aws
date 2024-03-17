41a73753bf9771543513847b68ecc212c71e37d0
Aigul Ysakova <ysakova90@gmail.com>
2024-03-17 18:08:22 -0500

    a

--- RDS/main.tf
+++ RDS/main.tf
@@ -1,3 +1,10 @@
+data "aws_caller_identity" "current" {}
+
+
+locals {
+  account_id = data.aws_caller_identity.current.account_id
+}
+
 data "terraform_remote_state" "backend" {
   backend = "s3"
   config {
