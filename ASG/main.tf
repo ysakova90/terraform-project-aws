57684c2ebbbd3b1bc16b8cd121a2450da424b5b3
Aigul Ysakova <ysakova90@gmail.com>
2024-03-17 03:20:43 -0500

    added

---
+++ ASG/main.tf
@@ -0,0 +1,45 @@
+resource "aws_launch_template" "server" {
+  name                   = "project-asg"
+  image_id               = "ami-01f570b45fc279a29"
+  instance_type          = "t2.micro"
+  user_data              = base64encode(file("user_data.sh"))
+  vpc_security_group_ids =  [
+     aws_security_group.ec2-sg.id
+  ]
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "project-team1"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+ # Allow HTTP/HTTP from ALL
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow HTTP/HTTPS from ALL
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow All Outbound
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+   
+
