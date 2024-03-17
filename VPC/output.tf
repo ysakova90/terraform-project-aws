e78b1a9711d96fd509be81b16a68423429a33ab9
Aigul Ysakova <ysakova90@gmail.com>
2024-03-16 21:50:48 -0500

    added

--- VPC/output.tf
+++ VPC/output.tf
@@ -10,3 +10,12 @@ output "public_subnet2" {
 output "public_subnet3" {
   value = aws_subnet.public_subnet3.id
 }
+output "private_subnet1" {
+  value = aws_subnet.public_subnet1.id
+}
+output "private_subnet2" {
+  value = aws_subnet.public_subnet2.id
+}
+output "private_subnet3" {
+  value = aws_subnet.public_subnet3.id
+}
