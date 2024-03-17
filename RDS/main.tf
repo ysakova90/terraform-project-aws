ad8dbe02eaba9a14c7f5c3c8295a2299ddf2bdde
Aigul Ysakova <ysakova90@gmail.com>
2024-03-16 22:22:50 -0500

    added

--- RDS/main.tf
+++ RDS/main.tf
@@ -1,5 +1,5 @@
 ##Create subnet group for RDS
 resource "aws_db_subnet_group" "db_subnet" {
   name       = "db_subnet"
-  subnet_ids = [aws_subnet.private_subnet1.id, aws_subnet.private_subnet2.id, aws_subnet.private_subnet3.id]
-}
+  subnet_ids = data.terraform_remote_state.vpc.outputs.private_subnets
+}  
