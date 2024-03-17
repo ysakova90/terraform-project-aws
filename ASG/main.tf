2074e696396ed2923d3c72483d7bc8f1dbe9a5ee
Aigul Ysakova <ysakova90@gmail.com>
2024-03-17 03:07:27 -0500

    c

--- ASG/main.tf
+++ ASG/main.tf
@@ -1,39 +1,34 @@
-resource "aws_security_group" "ec2-sg" {
+resource "aws_security_group" "allow_tls" {
   name        = "project-team1"
-  description = "Allow SSH , HTTP and HTTPS inbound  from Frontend app"
+  description = "Allow TLS inbound traffic and all outbound traffic"
   vpc_id      = aws_vpc.main.id
-}
 
+ # Allow HTTP/HTTPS from ALL
   ingress {
-    description = "TLS from VPC"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "TLS from VPC"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "TLS from VPC"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # Allow HTTP/HTTPS from ALL
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow All Outbound
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+
    
 
 resource "aws_launch_template" "server" {
