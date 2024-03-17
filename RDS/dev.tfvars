498af4035feb02fdd69e350cece64826acd46799
Aigul Ysakova <ysakova90@gmail.com>
2024-03-16 20:14:40 -0500

    added

--- RDS/dev.tfvars
+++ RDS/dev.tfvars
@@ -1,8 +1,1 @@
 region = "us-east-1"
-private_subanet1_id = subnet-05f786cd119e00881
-private_subanet2_id = subnet-0ea172c51bf0993dc
-private_subanet3_id = subnet-0605c166b40064502
-tags = {
-  Name      = "Terraform-project-rds"
-  createdBy = "Team-1"
-}
--- RDS/main.tf
+++ RDS/main.tf
@@ -1,5 +1,5 @@
 ##Create subnet group for RDS
 resource "aws_db_subnet_group" "db_subnet" {
   name       = "db_subnet"
-  subnet_ids = var.private_subnet_ids
+  subnet_ids = [aws_subnet.private_subnet1.id, aws_subnet.private_subnet2.id, aws_subnet.private_subnet3.id]
 }
