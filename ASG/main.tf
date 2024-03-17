3d56c54be98719d82692607b4f549a1cb3386474
Aigul Ysakova <ysakova90@gmail.com>
2024-03-17 02:59:10 -0500

    a

--- ASG/main.tf
+++ ASG/main.tf
@@ -32,8 +32,7 @@ resource "aws_security_group" "ec2-sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-    tags = var.tags
-  
+   
 
 resource "aws_launch_template" "server" {
   name                   = "project-asg"
