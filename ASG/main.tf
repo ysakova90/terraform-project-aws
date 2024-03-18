004538d27003f15ecf5fb4314888a13633a7e9c6
EC2 Default User <ec2-user@ip-172-31-25-187.ec2.internal>
2024-03-18 21:02:39 +0000

    added

---
+++ ASG/main.tf
@@ -0,0 +1,47 @@
+data "terraform_remote_state" "backend" {
+  backend = "s3"
+   config  = {
+    bucket = "team1-aws-storage"
+    key = "vpc-statefile"
+    region = "us-east-1"
+  }
+} 
+ resource "aws_security_group" "ec2-sg" {
+  name        = "project-team"
+  description = "EC2 Instance Security Group"
+  vpc_id      =   data.terraform_remote_state.backend.outputs.vpc_id
+
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
