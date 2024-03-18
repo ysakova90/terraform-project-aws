9749f2585c874944f716d61d1815e323e104c1a8
Aigul Ysakova <ysakova90@gmail.com>
2024-03-17 21:33:08 -0500

    c

--- RDS/main.tf
+++ RDS/main.tf
@@ -11,9 +11,9 @@ data "terraform_remote_state" "backend" {
 
 resource "aws_db_subnet_group" "RDS_subnet_grp" {
   subnet_ids = [ 
-    data.terraform_remote_state.backend.VPC.outputs.private_subnet1,
-    data.terraform_remote_state.backend.VPC.outputs.private_subnet2,
-    data.terraform_remote_state.backend.VPC.outputs.private_subnet3
+    data.terraform_remote_state.backend.outputs.private_subnet1,
+    data.terraform_remote_state.backend.outputs.private_subnet2,
+    data.terraform_remote_state.backend.outputs.private_subnet3
   ]
 
 }
