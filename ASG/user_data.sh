d51e59675eae8172de0ec6261c4323cb08595db2
EC2-user <ec2-user@ip-172-31-80-8.ec2.internal>
2024-03-19 08:16:00 +0000

    a

--- ASG/user_data.sh
+++ ASG/user_data.sh
@@ -1,24 +1,22 @@
 
-#!/bin/bash
+
+#Change these values and keep in safe place
+db_root_password=PassWord4-root
+db_username=dbuser
+db_user_password=dbuserpass
+db_name=wordpress_db
+
+# install LAMP Server
 yum update -y
-
-#install apache server and mysql client
+#install apache server
 yum install -y httpd
-yum install -y mysql
+ 
+#since amazon ami 2018 is no longer supported ,to install latest php and mysql we have to do some tricks.
+#first enable php7.xx from  amazon-linux-extra and install it
 
-yum install nfs-utils -y -q
-
-# Mounting Efs 
-sudo mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${efs_dns_name}:/  /var/www/html
-# Making Mount Permanent
-echo '${efs_dns_name}:/ /var/www/html nfs4 defaults,vers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport 0 0' >> /etc/fstab
-
-
-# first enable php7.xx from  amazon-linux-extra and install it
 amazon-linux-extras enable php7.4
 yum clean metadata
 yum install -y php php-{pear,cgi,common,curl,mbstring,gd,mysqlnd,gettext,bcmath,json,xml,fpm,intl,zip,imap,devel}
-
 #install imagick extension
 yum -y install gcc ImageMagick ImageMagick-devel ImageMagick-perl
 pecl install imagick
@@ -30,8 +28,16 @@ extension=imagick
 EOF
 
 systemctl restart php-fpm.service
-systemctl start  httpd
 
+#and download mysql package to yum  and install mysql server from yum
+yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+yum localinstall -y https://dev.mysql.com/get/mysql57-community-release-el7-11.noarch.rpm
+yum install -y mysql-community-server
+
+
+
+systemctl start  httpd
+systemctl start mysqld
 
 # Change OWNER and permission of directory /var/www
 usermod -a -G apache ec2-user
@@ -39,53 +45,50 @@ chown -R ec2-user:apache /var/www
 find /var/www -type d -exec chmod 2775 {} \;
 find /var/www -type f -exec chmod 0664 {} \;
 
-
-#Download wordpress package and extract
+# Download wordpress package and extract
 wget https://wordpress.org/latest.tar.gz
 tar -xzf latest.tar.gz
 cp -r wordpress/* /var/www/html/
+
+# AUTOMATIC mysql_secure_installation
+# change permission of error log file to extract initial root password 
+chown  ec2-user:apache /var/log/mysqld.log
+temppassword=$(grep 'temporary password' /var/log/mysqld.log | grep -o ".\{12\}$")
+chown  mysql:mysql /var/log/mysqld.log
+
+#change root password to db_root_password
+ mysql -p$temppassword --connect-expired-password  -e "SET PASSWORD FOR root@localhost = PASSWORD('$db_root_password');FLUSH PRIVILEGES;" 
+mysql -p'$db_root_password'  -e "DELETE FROM mysql.user WHERE User='';"
+mysql -p'$db_root_password' -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
+
+
+# Create database user and grant privileges
+mysql -u root -p"$db_root_password" -e "GRANT ALL PRIVILEGES ON *.* TO '$db_username'@'localhost' IDENTIFIED BY '$db_user_password';FLUSH PRIVILEGES;"
+
+# Create database
+mysql -u $db_username -p"$db_user_password" -e "CREATE DATABASE $db_name;"
+
+# Create wordpress configuration file and update database value
 cd /var/www/html
 cp wp-config-sample.php wp-config.php
+
 sed -i "s/database_name_here/$db_name/g" wp-config.php
 sed -i "s/username_here/$db_username/g" wp-config.php
-sed -i "s/password_here/$db_user_password/g" wp-config.php
-sed -i "s/localhost/$db_RDS/g" wp-config.php
+sed -i "s/password_here/$db_user_pass/g" wp-config.php
 cat <<EOF >>/var/www/html/wp-config.php
+
 define( 'FS_METHOD', 'direct' );
-define('WP_MEMORY_LIMIT', '128M');
+define('WP_MEMORY_LIMIT', '256M');
 EOF
-
-
 
 # Change permission of /var/www/html/
 chown -R ec2-user:apache /var/www/html
 chmod -R 774 /var/www/html
 
-
-
 #  enable .htaccess files in Apache config using sed command
 sed -i '/<Directory "\/var\/www\/html">/,/<\/Directory>/ s/AllowOverride None/AllowOverride all/' /etc/httpd/conf/httpd.conf
 
-#Make apache  autostart and restart apache
+#Make apache and mysql to autostart and restart apache
 systemctl enable  httpd.service
+systemctl enable mysqld.service
 systemctl restart httpd.service
-echo WordPress Installed
-
-
-## Install Redis Cache ##
-
-sudo yum -y install gcc make # install GCC compiler
-cd /usr/local/src
-sudo wget http://download.redis.io/redis-stable.tar.gz
-sudo tar xvzf redis-stable.tar.gz
-sudo rm -f redis-stable.tar.gz
-cd redis-stable
-sudo yum groupinstall "Development Tools" -y
-sudo make distclean
-sudo make
-sudo yum install -y tcl
-
-sudo cp src/redis-server /usr/local/bin/
-sudo cp src/redis-cli /usr/local/bin/
-
-redis-server --daemonize yes
